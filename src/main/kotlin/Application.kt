7a2e0fd1a99a15667fd38dbda18e8c59561029ad
k-dgier <kitten.diger@gmail.com>
2025-05-15 00:26:29 +0900

    메인 애플리케이션 패키지 이동

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,5 +1,11 @@
-package com.mashup.samples
+package com.mashup
 
+import com.mashup.samples.configureAdministration
+import com.mashup.samples.configureDatabases
+import com.mashup.samples.configureHTTP
+import com.mashup.samples.configureMonitoring
+import com.mashup.samples.configureRouting
+import com.mashup.samples.configureSerialization
 import io.ktor.server.application.Application
 import io.ktor.server.netty.EngineMain
 
