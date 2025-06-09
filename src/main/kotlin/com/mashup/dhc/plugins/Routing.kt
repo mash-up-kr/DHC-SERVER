cff9ed9f62a4b85cdb81dc1d1ce704ec329234cb
seongahjo <ah.jo@navercorp.com>
2025-06-09 18:03:44 +0900

    회원가입 API 추가

--- src/main/kotlin/com/mashup/dhc/plugins/Routing.kt
+++ src/main/kotlin/com/mashup/dhc/plugins/Routing.kt
@@ -1,14 +1,18 @@
 package com.mashup.com.mashup.dhc.plugins
 
-import com.mashup.dhc.routes.sampleRoute
+import com.mashup.com.mashup.dhc.domain.service.UserService
+import com.mashup.dhc.routes.register
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.autohead.AutoHeadResponse
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.routing.routing
 
-fun Application.configureRouting() {
+fun Application.configureRouting(userService: UserService) {
     install(AutoHeadResponse)
+    install(ContentNegotiation) { json() }
     routing {
-        sampleRoute()
+        register(userService)
     }
 }
