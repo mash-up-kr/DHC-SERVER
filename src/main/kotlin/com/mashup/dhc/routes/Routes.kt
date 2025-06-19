da3ffa90a1eac0f82f477187f659781d4b800267
seongahjo <ah.jo@navercorp.com>
2025-06-19 16:49:20 +0900

    라우팅 path 수정

--- src/main/kotlin/com/mashup/dhc/routes/Routes.kt
+++ src/main/kotlin/com/mashup/dhc/routes/Routes.kt
@@ -143,7 +143,7 @@ data class HomeViewResponse(
 )
 
 private fun Route.endToday(userService: UserService) {
-    post("/{userId}/done") {
+    post("/done") {
         val userId = call.pathParameters["userId"]!!
         val request = call.receive<EndTodayMissionRequest>()
 
@@ -196,7 +196,7 @@ data class ToggleMissionResponse(
 )
 
 private fun Route.myPage(userService: UserService) {
-    get("/{userId}/myPage") {
+    get("/myPage") {
         val userId = call.pathParameters["userId"]!!
         val user = userService.getUserById(userId)
 
